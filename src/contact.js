c42824b76195f10c40a5545348d66224e0b45e27
avedyer <avedyer@gmail.com>
2021-11-27 10:04:33 -0500

    build basic contact form

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,26 @@
 export default function contact() {
-    const contact = document.createElement('form')
+    const contact = document.createElement('form');
+    contact.classList.add('contact');
+
+    const textbox = document.createElement('textarea');
+    textbox.setAttribute('placeholder', 'Comments? Questions? Compliments? Leave your message here!');
+    textbox.setAttribute('rows', "16");
+    textbox.classList.add('textbox');
+
+    const submit = document.createElement('input');
+    submit.setAttribute('type', 'button');
+    submit.value = 'Send';
+    submit.classList.add('send');
+
+    const received = document.createElement('p');
+    received.innerHTML = "Thanks! We'll reply shortly if needed.";
+    received.classList.add('invisible', 'received');
+
+    submit.onclick = () => {
+        received.classList.remove('invisible');
+    }
+
+    contact.append(textbox, submit, received);
+
     return contact
 }
