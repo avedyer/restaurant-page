81f899f0a94bdb8ed1858f7d2759f7e416bcc028
avedyer <avedyer@gmail.com>
2021-11-28 12:46:53 -0500

    style work added contact info

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,17 @@
 export default function contact() {
-    const contact = document.createElement('form');
+    const contact = document.createElement('div');
     contact.classList.add('contact', 'blueBg', 'bottomBorder');
+
+    const infoContainer = document.createElement('div');
+    infoContainer.classList.add('infoBox');
+
+    const phoneElement = document.createElement('p');
+    phoneElement.innerHTML = "Call: 443-274-1853";
+
+    const emailElement = document.createElement('p');
+    emailElement.innerHTML = "Email: thebigdonut@gemail.com";
+
+    infoContainer.append(phoneElement, emailElement);
 
     const textbox = document.createElement('textarea');
     textbox.setAttribute('placeholder', 'Comments? Questions? Compliments? Leave your message here!');
@@ -19,12 +30,12 @@ export default function contact() {
     submit.onclick = () => {
         if(received.parentNode){
             received.parentNode.removeChild(received);
+            console.log(textbox.value);
         }
         contact.append(received);
-        textbox.value = '';
     }
 
-    contact.append(textbox, submit);
+    contact.append(infoContainer, textbox, submit);
 
     return contact
 }
--- src/home.js
+++ src/home.js
@@ -1,18 +1,44 @@
 import logo from './logo.png';
 
 export default function home () {
-    const home = document.createElement('article');
-    home.classList.add('blueBg', 'bottomBorder');
+    const home = document.createElement('div');
+    home.classList.add('blueBg', 'bottomBorder', 'home');
 
-    const paragraph = document.createElement('p');
-    const description = "(RN) combines flavors & inspiration from the Far East & the West to create what we think is the best! Home to the original (recipe), we specialize in pizzas, as well as handmade appetizers, sandwiches, salads, and gluten friendly, vegetarian, & vegan selections. Feel free to indulge in a tiki drink or craft beer with our fantastic unique cuisine. Cheers!";
+    const aboutElement = document.createElement('p');
+    aboutElement.classList.add('about');
+    aboutElement.innerHTML = "The best donuts in Beach City, right off the Boardwalk!"
 
-    paragraph.innerHTML = description;
-    home.appendChild(paragraph);
+    const infoContainer = document.createElement('div');
+    infoContainer.classList.add('infoGrid');
+
+    const hoursContainer = document.createElement('div');
+    hoursContainer.classList.add('infoCell')
+
+    const hoursHeaderElement = document.createElement('h3');
+    hoursHeaderElement.innerHTML = "Hours"
+
+    const hoursElement = document.createElement('p');
+    hoursElement.innerHTML = ("MON-THU: 11am-7pm</br>FRI-SAT: 11am-9pm</br>SUN: 9am-4pm");
+
+    hoursContainer.append(hoursHeaderElement, hoursElement);
+
+    const locationContainer = document.createElement('div');
+    locationContainer.classList.add('infoCell')
+
+    const locationHeaderElement = document.createElement('h3');
+    locationHeaderElement.innerHTML = "Location";
+
+    const locationElement = document.createElement('p');
+    locationElement.innerHTML = ("16 Boardwalk Drive</br>Beach City, Delmarva, 93204")
+
+    locationContainer.append(locationHeaderElement, locationElement);
+
+    infoContainer.append(hoursContainer, locationContainer);
 
     const logoImg = new Image();
     logoImg.src = logo;
-    home.appendChild(logoImg);
+
+    home.append(aboutElement, infoContainer, logoImg);
 
     return home;
 }
